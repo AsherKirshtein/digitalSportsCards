2f0cf3ef1c57a4d9dd0f3a5daa9ca4fe8a11854f
AsherKirshtein <76977142+AsherKirshtein@users.noreply.github.com>
2024-01-24 12:14:36 -0500

    Delete celeryconfig.py

--- celeryconfig.py
+++
@@ -1,8 +0,0 @@
-# Celery Settings 
-broker_url = 'redis://127.0.0.1:6379/0' # URL for the message broker,
-result_backend = 'redis://127.0.0.1:6379/0'
-task_serializer = 'json'
-accept_content = ['json']  # Content type accepted
-bind = '0.0.0.0'
-timezone = 'UTC'
-enable_utc = True
